1172dd99a11c1a961b90e03a8d399438e66e3824
Stewart Celani <me@stewartcelani.com>
2024-12-13 12:14:11 +1030

    Fixed MediatR FluentValidation pipeline not registering validators properly.

--- AdventureWorks.API/Program.cs
+++ AdventureWorks.API/Program.cs
@@ -2,20 +2,23 @@ using AdventureWorks.API.Application.Common.Data;
 using AdventureWorks.API.Application.Common.Extensions;
 using AdventureWorks.API.Application.Common.Pipelines;
 using AdventureWorks.API.Application.Common.Settings;
+using FluentValidation;
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionStrings = builder.Configuration.BindAndValidate<ConnectionStrings, ConnectionStringsValidator>();
-builder.Services.AddSingleton(connectionStrings);
-
-builder.Services.AddTransient<IDbConnectionFactory, SqlDbConnectionFactory>();
-
+// MediatR
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 builder.Services
     .AddMediatR(options => { options.RegisterServicesFromAssemblyContaining(typeof(Program)); })
     .AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>))
     .AddScoped(typeof(IPipelineBehavior<,>), typeof(AuditLoggingBehaviour<,>));
+
+// Connection Strings and Dapper
+var connectionStrings = builder.Configuration.BindAndValidate<ConnectionStrings, ConnectionStringsValidator>();
+builder.Services.AddSingleton(connectionStrings);
+builder.Services.AddTransient<IDbConnectionFactory, SqlDbConnectionFactory>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
